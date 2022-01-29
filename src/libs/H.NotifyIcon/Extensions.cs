e0459bf7fca6d2e4db429716463da65ea2e44c04
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-01-29 11:34:30 +1000

    feat: Moved some platform agnostic extensions to .Net Standard library.

---
+++ src/libs/H.NotifyIcon/Extensions.cs
@@ -0,0 +1,122 @@
+﻿// hardcodet.net NotifyIcon for WPF
+// Copyright (c) 2009 - 2020 Philipp Sumi
+// Contact and Information: http://www.hardcodet.net
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Code Project Open License (CPOL);
+// either version 1.0 of the License, or (at your option) any later
+// version.
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// THIS COPYRIGHT NOTICE MAY NOT BE REMOVED FROM THIS FILE
+
+
+using System;
+using Hardcodet.Wpf.TaskbarNotification.Interop;
+
+namespace Hardcodet.Wpf.TaskbarNotification
+{
+    /// <summary>
+    /// Util and extension methods.
+    /// </summary>
+    public static class Extensions
+    {
+        #region GetBalloonFlag
+
+        /// <summary>
+        /// Gets a <see cref="BalloonFlags"/> enum value that
+        /// matches a given <see cref="BalloonIcon"/>.
+        /// </summary>
+        public static BalloonFlags GetBalloonFlag(this BalloonIcon icon)
+        {
+            switch (icon)
+            {
+                case BalloonIcon.None:
+                    return BalloonFlags.None;
+                case BalloonIcon.Info:
+                    return BalloonFlags.Info;
+                case BalloonIcon.Warning:
+                    return BalloonFlags.Warning;
+                case BalloonIcon.Error:
+                    return BalloonFlags.Error;
+                default:
+                    throw new ArgumentOutOfRangeException("icon");
+            }
+        }
+
+        #endregion
+
+        #region evaluate listings
+
+        /// <summary>
+        /// Checks a list of candidates for equality to a given
+        /// reference value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The evaluated value.</param>
+        /// <param name="candidates">A liste of possible values that are
+        /// regarded valid.</param>
+        /// <returns>True if one of the submitted <paramref name="candidates"/>
+        /// matches the evaluated value. If the <paramref name="candidates"/>
+        /// parameter itself is null, too, the method returns false as well,
+        /// which allows to check with null values, too.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="candidates"/>
+        /// is a null reference.</exception>
+        public static bool Is<T>(this T value, params T[] candidates)
+        {
+            if (candidates == null) return false;
+
+            foreach (var t in candidates)
+            {
+                if (value.Equals(t)) return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region match MouseEvent to PopupActivation
+
+        /// <summary>
+        /// Checks if a given <see cref="PopupActivationMode"/> is a match for
+        /// an effectively pressed mouse button.
+        /// </summary>
+        public static bool IsMatch(this MouseEvent me, PopupActivationMode activationMode)
+        {
+            switch (activationMode)
+            {
+                case PopupActivationMode.LeftClick:
+                    return me == MouseEvent.IconLeftMouseUp;
+                case PopupActivationMode.RightClick:
+                    return me == MouseEvent.IconRightMouseUp;
+                case PopupActivationMode.LeftOrRightClick:
+                    return me.Is(MouseEvent.IconLeftMouseUp, MouseEvent.IconRightMouseUp);
+                case PopupActivationMode.LeftOrDoubleClick:
+                    return me.Is(MouseEvent.IconLeftMouseUp, MouseEvent.IconDoubleClick);
+                case PopupActivationMode.DoubleClick:
+                    return me.Is(MouseEvent.IconDoubleClick);
+                case PopupActivationMode.MiddleClick:
+                    return me == MouseEvent.IconMiddleMouseUp;
+                case PopupActivationMode.All:
+                    //return true for everything except mouse movements
+                    return me != MouseEvent.MouseMove;
+                default:
+                    throw new ArgumentOutOfRangeException("activationMode");
+            }
+        }
+
+        #endregion
+    }
+}
