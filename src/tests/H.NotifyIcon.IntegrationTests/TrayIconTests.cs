25bbe9d7cf75e8f37a7e001a942afd1197fc9253
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-04-01 19:34:39 +1000

    test: Added IntergrationTests.

---
+++ src/tests/H.NotifyIcon.IntegrationTests/TrayIconTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using H.NotifyIcon.Interop;
+
+namespace H.NotifyIcon.IntegrationTests;
+
+[TestClass]
+public class TrayIconTests
+{
+    [TestMethod]
+    public async Task SimpleTest()
+    {
+        using var trayIcon = new TrayIcon(false);
+        using var iconStream = H.Resources.Red_ico.AsStream();
+        using var icon = new Icon(iconStream);
+        trayIcon.SetIcon(icon.Handle);
+        trayIcon.SetToolTip(nameof(SimpleTest));
+
+        await Task.Delay(TimeSpan.FromSeconds(15));
+    }
+}
