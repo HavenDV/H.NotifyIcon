63a0f2e812d12288b3ed4aea817c330c8dde6370
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-04-02 19:42:23 +1000

    apps: Added WinUI.Windowless app.

---
+++ src/apps/H.NotifyIcon.Apps.WinUI.Windowless/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace H.NotifyIcon.Apps;
+
+public sealed partial class App
+{
+    #region Properties
+
+    public static TaskbarIcon? TaskbarIcon { get; private set; }
+
+    #endregion
+
+    #region Constructors
+
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    #endregion
+
+    #region Event Handlers
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        TaskbarIcon = (TaskbarIcon)Resources["TaskbarIcon"];
+    }
+
+    #endregion
+}
---
+++ src/apps/H.NotifyIcon.Apps.WinUI.Windowless/TaskbarDataContext.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using System.Windows.Input;
+using WinUIEx;
+
+namespace H.NotifyIcon.Apps;
+
+public class TaskbarDataContext
+{
+    private Window? Window { get; set; }
+
+    public ICommand ShowHideWindowCommand
+    {
+        get
+        {
+            var command = new XamlUICommand();
+            command.ExecuteRequested += (sender, e) =>
+            {
+                if (Window == null)
+                {
+                    Window = new Window();
+                    Window.Activate();
+                    return;
+                }
+
+                if (Window.Visible)
+                {
+                    Window.Hide();
+                }
+                else
+                {
+                    Window.Show();
+                }
+            };
+
+            return command;
+        }
+    }
+
+    public ICommand ExitApplicationCommand
+    {
+        get
+        {
+            var command = new XamlUICommand();
+            command.ExecuteRequested += (sender, e) =>
+            {
+                App.TaskbarIcon?.Dispose();
+
+                if (Window != null)
+                {
+                    Window.Close();
+                }
+                else
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+            };
+
+            return command;
+        }
+    }
+}
