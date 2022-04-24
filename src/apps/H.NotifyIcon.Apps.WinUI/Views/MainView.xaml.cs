c054df3d8d493a614d70ad698fa4f5eefe89b9a1
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-04-25 00:11:23 +1000

    apps(WinUI): Added Notifications example.

--- src/apps/H.NotifyIcon.Apps.WinUI/Views/MainView.xaml.cs
+++ src/apps/H.NotifyIcon.Apps.WinUI/Views/MainView.xaml.cs
@@ -5,5 +5,23 @@ public sealed partial class MainView
     public MainView()
     {
         InitializeComponent();
+
+        NavigationView.ItemInvoked += NavigationView_ItemInvoked;
+    }
+
+    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        var options = new FrameNavigationOptions
+        {
+            TransitionInfoOverride = args.RecommendedNavigationTransitionInfo,
+        };
+
+        switch (((string)args.InvokedItem))
+        {
+            case "Notifications":
+                _ = NavigationViewFrame.NavigateToType(typeof(NotificationView), null, options);
+                ((NotificationView)NavigationViewFrame.Content).TaskbarIcon = TaskbarIcon;
+                break;
+        }
     }
 }
---
+++ src/apps/H.NotifyIcon.Apps.WinUI/Views/NotificationView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using H.NotifyIcon.Core;
+
+namespace H.NotifyIcon.Apps.Views;
+
+public sealed partial class NotificationView
+{
+    public TaskbarIcon? TaskbarIcon { get; set; }
+
+    public NotificationView()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowBotificationButton_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedIcon = (Type.SelectedItem as RadioButton)?.Content;
+
+        TaskbarIcon?.ShowNotification(
+            title: TitleTextBox.Text,
+            message: MessageTextBox.Text,
+            icon: selectedIcon switch
+            {
+                "None" => NotificationIcon.None,
+                "Information" => NotificationIcon.Info,
+                "Warning" => NotificationIcon.Warning,
+                "Error" => NotificationIcon.Error,
+                _ => NotificationIcon.None,
+            },
+            customIcon: selectedIcon switch
+            {
+                "Custom" => TaskbarIcon.Icon,
+                _ => null,
+            },
+            //largeIcon: LargeIconCheckBox.IsChecked ?? false,
+            sound: SoundCheckBox.IsChecked ?? true,
+            respectQuietTime: true,
+            realtime: false,
+            timeout: null);
+    }
+
+    private void ClearNotificationsButton_Click(object sender, RoutedEventArgs e)
+    {
+        TaskbarIcon?.ClearNotifications();
+    }
+}
