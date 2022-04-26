d9794d1037542445ec7b150475a6b021f6c2a5be
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-04-26 20:23:51 +1000

    apps(WinUI): Refactored Windowless app.

--- src/apps/H.NotifyIcon.Apps.WinUI.Windowless/App.xaml.cs
+++ src/apps/H.NotifyIcon.Apps.WinUI.Windowless/App.xaml.cs
@@ -4,7 +4,8 @@ public sealed partial class App
 {
     #region Properties
 
-    public static TaskbarIcon? TaskbarIcon { get; private set; }
+    public static TaskbarIcon? TrayIcon { get; private set; }
+    public static Window? Window { get; set; }
 
     #endregion
 
@@ -21,8 +22,44 @@ public sealed partial class App
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        TaskbarIcon = (TaskbarIcon)Resources["TaskbarIcon"];
-        TaskbarIcon.ForceCreate();
+        InitializeTrayIcon();
+    }
+
+    private void InitializeTrayIcon()
+    {
+        var showHideWindowCommand = (XamlUICommand)Resources["ShowHideWindowCommand"];
+        showHideWindowCommand.ExecuteRequested += ShowHideWindowCommand_ExecuteRequested;
+
+        var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"];
+        exitApplicationCommand.ExecuteRequested += ExitApplicationCommand_ExecuteRequested;
+
+        TrayIcon = (TaskbarIcon)Resources["TrayIcon"];
+        TrayIcon.ForceCreate();
+    }
+
+    private void ShowHideWindowCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+    {
+        if (Window == null)
+        {
+            Window = new Window();
+            Window.Show();
+            return;
+        }
+
+        if (Window.Visible)
+        {
+            Window.Hide();
+        }
+        else
+        {
+            Window.Show();
+        }
+    }
+
+    private void ExitApplicationCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+    {
+        TrayIcon?.Dispose();
+        Window?.Close();
     }
 
     #endregion
