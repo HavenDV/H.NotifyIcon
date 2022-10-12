67df60a843a87854c95d6348491f6eece952e17b
Espen Skaufel <espen.skaufel@verent.no>
2022-10-12 16:38:35 +0200

    Removed unnecessary usings in PopupSubMenu

--- src/libs/H.NotifyIcon/PopupMenus/PopupSubMenu.cs
+++ src/libs/H.NotifyIcon/PopupMenus/PopupSubMenu.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using H.NotifyIcon.Core;
-
-namespace H.NotifyIcon.Core;
+﻿namespace H.NotifyIcon.Core;
 
 /// <inheritdoc/>
 public class PopupSubMenu : PopupItem
