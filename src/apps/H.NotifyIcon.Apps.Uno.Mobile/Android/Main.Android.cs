21414a268ad20195e7ecb83b4adf61741072a39a
Konstantin S <havendv@gmail.com>
2022-10-20 11:50:48 +0500

    apps: Added Uno.Mobile app.

---
+++ src/apps/H.NotifyIcon.Apps.Uno.Mobile/Android/Main.Android.cs
@@ -0,0 +1,32 @@
+﻿using Android.Runtime;
+using Com.Nostra13.Universalimageloader.Core;
+
+namespace H.NotifyIcon.Apps
+{
+    [global::Android.App.ApplicationAttribute(
+		Label = "@string/ApplicationName",
+		LargeHeap = true,
+		HardwareAccelerated = true,
+		Theme = "@style/AppTheme"
+	)]
+	public class Application : Windows.UI.Xaml.NativeApplication
+	{
+		public Application(IntPtr javaReference, JniHandleOwnership transfer)
+			: base(() => new App(), javaReference, transfer)
+		{
+			ConfigureUniversalImageLoader();
+		}
+
+		private void ConfigureUniversalImageLoader()
+		{
+			// Create global configuration and initialize ImageLoader with this config
+			ImageLoaderConfiguration config = new ImageLoaderConfiguration
+				.Builder(Context)
+				.Build();
+
+			ImageLoader.Instance.Init(config);
+
+			ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
+		}
+	}
+}
---
+++ src/apps/H.NotifyIcon.Apps.Uno.Mobile/Android/MainActivity.Android.cs
@@ -0,0 +1,14 @@
+﻿using Android.Views;
+
+namespace H.NotifyIcon.Apps
+{
+    [Activity(
+			MainLauncher = true,
+			ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+	public class MainActivity : Windows.UI.Xaml.ApplicationActivity
+	{
+	}
+}
+
