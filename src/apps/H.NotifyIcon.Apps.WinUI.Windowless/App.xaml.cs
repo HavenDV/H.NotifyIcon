8e897754b9cf35a7ecea1010e504d046911231ed
Renn <8340896+AkazaRenn@users.noreply.github.com>
2023-01-20 14:43:46 -0500

    Add sample code

--- src/apps/H.NotifyIcon.Apps.WinUI.Windowless/App.xaml.cs
+++ src/apps/H.NotifyIcon.Apps.WinUI.Windowless/App.xaml.cs
@@ -7,6 +7,8 @@ public sealed partial class App
     public static TaskbarIcon? TrayIcon { get; private set; }
     public static Window? Window { get; set; }
 
+    public static bool option = false;
+
     #endregion
 
     #region Constructors
@@ -27,39 +29,52 @@ public sealed partial class App
 
     private void InitializeTrayIcon()
     {
-        var showHideWindowCommand = (XamlUICommand)Resources["ShowHideWindowCommand"];
-        showHideWindowCommand.ExecuteRequested += ShowHideWindowCommand_ExecuteRequested;
-
         var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"];
         exitApplicationCommand.ExecuteRequested += ExitApplicationCommand_ExecuteRequested;
 
+        var commandOne = (XamlUICommand)Resources["Command1"];
+        commandOne.ExecuteRequested += commandOne_ExecuteRequested;
+
         TrayIcon = (TaskbarIcon)Resources["TrayIcon"];
         TrayIcon.ForceCreate();
-    }
-
-    private void ShowHideWindowCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
-    {
-        if (Window == null)
-        {
-            Window = new Window();
-            Window.Show();
-            return;
-        }
-
-        if (Window.Visible)
-        {
-            Window.Hide();
-        }
-        else
-        {
-            Window.Show();
-        }
     }
 
     private void ExitApplicationCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
     {
         TrayIcon?.Dispose();
         Window?.Close();
+    }
+
+    private void commandOne_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+    {
+        option = !option;
+        var commandOne = (XamlUICommand)Resources["Command1"];
+        if (option)
+        {
+            var iconHostOne = (XamlUICommand)Resources["IconHost1"];
+            commandOne.IconSource = iconHostOne.IconSource;
+            // This way works but as you can see it kind of a bad practice,
+            // I would like to be able to do like below instead:
+            // commandOne.IconSource.SymbolIconSource.Symbol = "Cancel";
+        }
+        else
+        {
+            var iconHostTwo = (XamlUICommand)Resources["IconHost2"];
+            commandOne.IconSource = iconHostTwo.IconSource;
+        }
+        // Also, I'm hoping it is possible to have an option like below,
+        // to make it easier for to toggle multiple options altogether:
+        // 
+        //   < XamlUICommand
+        //       x: Key = "Command0"
+        //       Label = "Command0"
+        //       Description = "Command0"
+        // --->  CloseMenuAfterClick = "False"
+        //       >
+        //       < XamlUICommand.IconSource >
+        //           < SymbolIconSource Symbol = "Cancel" />
+        //       </ XamlUICommand.IconSource >
+        //   </ XamlUICommand >
     }
 
     #endregion
