32ca23cdb90f02d132a235ed9d29f6cadd423a05
Konstantin S <havendv@gmail.com>
2023-03-29 06:41:35 +0400

    refactor: Added DesktopWindowsManagerMethods.SetRoundedCorners.

---
+++ src/libs/H.NotifyIcon/Interop/DesktopWindowsManagerMethods.cs
@@ -0,0 +1,23 @@
+﻿namespace H.NotifyIcon.Interop;
+
+/// <summary>
+/// 
+/// </summary>
+internal static class DesktopWindowsManagerMethods
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="handle"></param>
+    [SupportedOSPlatform("windows6.0.6000")]
+    public static unsafe void SetRoundedCorners(nint handle)
+    {
+        var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+        
+        _ = PInvoke.DwmSetWindowAttribute(
+            hwnd: new HWND(value: handle),
+            dwAttribute: DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE,
+            pvAttribute: &preference,
+            cbAttribute: sizeof(uint));
+    }
+}
