04fecceaaf420e92586b5d02f1e72eabc1311e16
Konstantin S <havendv@gmail.com>
2023-09-20 23:52:01 +0400

    refactor: Added TaskbarIcon.Dispose.cs.

---
+++ src/libs/H.NotifyIcon.Shared/TaskbarIcon.Dispose.cs
@@ -0,0 +1,129 @@
+﻿namespace H.NotifyIcon;
+
+public partial class TaskbarIcon : IDisposable
+{
+    /// <summary>
+    /// Set to true as soon as <c>Dispose</c> has been invoked.
+    /// </summary>
+    public bool IsDisposed { get; private set; }
+    
+    [SupportedOSPlatform("windows5.1.2600")]
+    private void DisposeAfterExit()
+    {
+#if HAS_WPF
+        // register listener in order to get notified when the application closes
+        if (Application.Current != null)
+        {
+            Application.Current.Exit += OnExit;
+        }
+#endif
+    }
+
+    private void EnsureNotDisposed()
+    {
+        if (IsDisposed)
+        {
+#if HAS_MAUI            
+            throw new ObjectDisposedException(GetType().FullName);
+#else
+            throw new ObjectDisposedException(Name ?? nameof(TaskbarIcon));
+#endif
+        }
+    }
+
+    [SupportedOSPlatform("windows5.1.2600")]
+    private void OnExit(object sender, EventArgs e)
+    {
+        Dispose();
+    }
+
+    /// <summary>
+    /// This destructor will run only if the <see cref="Dispose()"/>
+    /// method does not get called. This gives this base class the
+    /// opportunity to finalize.
+    /// <para>
+    /// Important: Do not provide destructor in types derived from this class.
+    /// </para>
+    /// </summary>
+    [SupportedOSPlatform("windows5.1.2600")]
+    ~TaskbarIcon()
+    {
+        Dispose(false);
+    }
+
+    /// <summary>
+    /// Disposes the object.
+    /// </summary>
+    /// <remarks>This method is not virtual by design. Derived classes
+    /// should override <see cref="Dispose(bool)"/>.
+    /// </remarks>
+    [SupportedOSPlatform("windows5.1.2600")]
+#if HAS_UNO
+    public new void Dispose()
+#else
+    public void Dispose()
+#endif
+    {
+        Dispose(true);
+
+        // This object will be cleaned up by the Dispose method.
+        // Therefore, you should call GC.SuppressFinalize to
+        // take this object off the finalization queue
+        // and prevent finalization code for this object
+        // from executing a second time.
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Closes the tray and releases all resources.
+    /// </summary>
+    /// <summary>
+    /// <c>Dispose(bool disposing)</c> executes in two distinct scenarios.
+    /// If disposing equals <c>true</c>, the method has been called directly
+    /// or indirectly by a user's code. Managed and unmanaged resources
+    /// can be disposed.
+    /// </summary>
+    /// <param name="disposing">If disposing equals <c>false</c>, the method
+    /// has been called by the runtime from inside the finalizer and you
+    /// should not reference other objects. Only unmanaged resources can
+    /// be disposed.</param>
+    /// <remarks>Check the <see cref="IsDisposed"/> property to determine whether
+    /// the method has already been called.</remarks>
+    [SupportedOSPlatform("windows5.1.2600")]
+    protected virtual void Dispose(bool disposing)
+    {
+        if (IsDisposed || !disposing)
+        {
+            return;
+        }
+
+        //lock (lockObject)
+        {
+            IsDisposed = true;
+
+#if HAS_WPF
+            // de-register application event listener
+            if (Application.Current != null)
+            {
+                Application.Current.Exit -= OnExit;
+            }
+#endif
+
+            // stop timers
+            SingleClickTimer.Dispose();
+#if HAS_WPF
+            balloonCloseTimer.Dispose();
+#endif
+
+#if !HAS_WPF && !HAS_UNO && !HAS_MAUI
+            ContextMenuWindow?.Close();
+            ContextMenuWindow = null;
+            ContextMenuWindowHandle = null;
+            ContextMenuAppWindow = null;
+#endif
+
+            TrayIcon.Dispose();
+            Icon?.Dispose();
+        }
+    }
+}
--- src/libs/H.NotifyIcon.Shared/TaskbarIcon.cs
+++ src/libs/H.NotifyIcon.Shared/TaskbarIcon.cs
@@ -22,7 +22,7 @@ namespace H.NotifyIcon;
 [UnsupportedOSPlatform("maccatalyst")]
 [SupportedOSPlatform("macos")]
 #endif
-public partial class TaskbarIcon : FrameworkElement, IDisposable
+public partial class TaskbarIcon : FrameworkElement
 {
     #region Properties
 
@@ -89,13 +89,7 @@ public partial class TaskbarIcon : FrameworkElement, IDisposable
         balloonCloseTimer = new Timer(CloseBalloonCallback);
 #endif
 
-#if HAS_WPF
-        // register listener in order to get notified when the application closes
-        if (Application.Current != null)
-        {
-            Application.Current.Exit += OnExit;
-        }
-#endif
+        DisposeAfterExit();
     }
 
     #endregion
@@ -146,136 +140,6 @@ public partial class TaskbarIcon : FrameworkElement, IDisposable
         // This seems to have been fixed in Windows 22598.1 but I'll leave it here for now
         //using var refreshTrayIcon = new TrayIcon(TrayIcon.CreateUniqueGuidForEntryAssembly("RefreshWorkaround"));
         //refreshTrayIcon.Create();
-    }
-
-    #endregion
-
-    #region Dispose / Exit
-
-    /// <summary>
-    /// Set to true as soon as <c>Dispose</c> has been invoked.
-    /// </summary>
-    public bool IsDisposed { get; private set; }
-
-
-    /// <summary>
-    /// Checks if the object has been disposed and
-    /// raises a <see cref="ObjectDisposedException"/> in case
-    /// the <see cref="IsDisposed"/> flag is true.
-    /// </summary>
-    private void EnsureNotDisposed()
-    {
-        if (IsDisposed)
-        {
-#if HAS_MAUI            
-            throw new ObjectDisposedException(GetType().FullName);
-#else
-            throw new ObjectDisposedException(Name ?? nameof(TaskbarIcon));
-#endif
-        }
-    }
-
-
-    /// <summary>
-    /// Disposes the class if the application exits.
-    /// </summary>
-    [SupportedOSPlatform("windows5.1.2600")]
-    private void OnExit(object sender, EventArgs e)
-    {
-        Dispose();
-    }
-
-
-    /// <summary>
-    /// This destructor will run only if the <see cref="Dispose()"/>
-    /// method does not get called. This gives this base class the
-    /// opportunity to finalize.
-    /// <para>
-    /// Important: Do not provide destructor in types derived from this class.
-    /// </para>
-    /// </summary>
-    [SupportedOSPlatform("windows5.1.2600")]
-    ~TaskbarIcon()
-    {
-        Dispose(false);
-    }
-
-
-    /// <summary>
-    /// Disposes the object.
-    /// </summary>
-    /// <remarks>This method is not virtual by design. Derived classes
-    /// should override <see cref="Dispose(bool)"/>.
-    /// </remarks>
-    [SupportedOSPlatform("windows5.1.2600")]
-#if HAS_UNO
-    public new void Dispose()
-#else
-    public void Dispose()
-#endif
-    {
-        Dispose(true);
-
-        // This object will be cleaned up by the Dispose method.
-        // Therefore, you should call GC.SuppressFinalize to
-        // take this object off the finalization queue
-        // and prevent finalization code for this object
-        // from executing a second time.
-        GC.SuppressFinalize(this);
-    }
-
-
-    /// <summary>
-    /// Closes the tray and releases all resources.
-    /// </summary>
-    /// <summary>
-    /// <c>Dispose(bool disposing)</c> executes in two distinct scenarios.
-    /// If disposing equals <c>true</c>, the method has been called directly
-    /// or indirectly by a user's code. Managed and unmanaged resources
-    /// can be disposed.
-    /// </summary>
-    /// <param name="disposing">If disposing equals <c>false</c>, the method
-    /// has been called by the runtime from inside the finalizer and you
-    /// should not reference other objects. Only unmanaged resources can
-    /// be disposed.</param>
-    /// <remarks>Check the <see cref="IsDisposed"/> property to determine whether
-    /// the method has already been called.</remarks>
-    [SupportedOSPlatform("windows5.1.2600")]
-    protected virtual void Dispose(bool disposing)
-    {
-        if (IsDisposed || !disposing)
-        {
-            return;
-        }
-
-        //lock (lockObject)
-        {
-            IsDisposed = true;
-
-#if HAS_WPF
-            // de-register application event listener
-            if (Application.Current != null)
-            {
-                Application.Current.Exit -= OnExit;
-            }
-#endif
-
-            // stop timers
-            SingleClickTimer?.Dispose();
-#if HAS_WPF
-            balloonCloseTimer.Dispose();
-#endif
-
-#if !HAS_WPF && !HAS_UNO && !HAS_MAUI
-            ContextMenuWindow?.Close();
-            ContextMenuWindow = null;
-            ContextMenuWindowHandle = null;
-            ContextMenuAppWindow = null;
-#endif
-
-            TrayIcon.Dispose();
-            Icon?.Dispose();
-        }
     }
 
     #endregion
