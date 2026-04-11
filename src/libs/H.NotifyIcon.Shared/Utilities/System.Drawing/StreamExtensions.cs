276d8448283cdebc724c6c65f67ba4e1a40c0452
Konstantin S. <havendv@gmail.com>
2026-04-11 05:12:37 +0400

    fix: convert png IconSource streams to icons

--- src/libs/H.NotifyIcon.Shared/Utilities/System.Drawing/StreamExtensions.cs
+++ src/libs/H.NotifyIcon.Shared/Utilities/System.Drawing/StreamExtensions.cs
@@ -8,8 +8,8 @@ internal static class StreamExtensions
     internal static Icon ToSmallIcon(this Stream stream)
     {
         var iconSize = IconUtilities.GetRequiredCustomIconSize(largeIcon: false).ScaleWithDpi();
-
-        return new Icon(stream, iconSize);
+        using var iconStream = stream.ToIconStream();
+        return new Icon(iconStream, iconSize);
     }
     
     [SupportedOSPlatform("windows")]
@@ -24,5 +24,35 @@ internal static class StreamExtensions
         using var image = System.Drawing.Image.FromStream(stream);
         
         return (image.Width, image.Height, System.Drawing.Image.GetPixelFormatSize(image.PixelFormat));
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static MemoryStream ToIconStream(this Stream stream)
+    {
+        if (stream == null)
+        {
+            throw new ArgumentNullException(nameof(stream));
+        }
+
+        using var buffer = new MemoryStream();
+        stream.CopyTo(buffer);
+
+        var data = buffer.ToArray();
+        var iconData = IsPng(data) ? data.ConvertPngToIco() : data;
+
+        return new MemoryStream(iconData, writable: false);
+    }
+
+    private static bool IsPng(byte[] data)
+    {
+        return data.Length >= 8 &&
+               data[0] == 0x89 &&
+               data[1] == 0x50 &&
+               data[2] == 0x4E &&
+               data[3] == 0x47 &&
+               data[4] == 0x0D &&
+               data[5] == 0x0A &&
+               data[6] == 0x1A &&
+               data[7] == 0x0A;
     }
 }
